3c6d9f4227e7320c71d44d8bdecd8fc2b0fff1ef
Gary Soedarsono <garysoed@gmail.com>
2021-05-02 11:49:36 -0700

    undefined

--- .mk.config.js
+++ .mk.config.js
@@ -5,3 +5,13 @@ declare({
     flags: ['link', 'gs-testing', 'gs-types', 'dev', 'devbase', 'moirai', 'nabu'],
   }),
 });
+
+declare({
+  name: 'commit',
+  as: serial({
+    cmds: [
+      single({bin: 'eslint', flags: ['**/*.ts']}),
+      single({bin: 'git', flags: ['commit', '-am', argv => argv[0]]}),
+    ],
+  }),
+});
